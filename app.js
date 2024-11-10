02257882181ab7ef51804f056654f8b2671ef8dd
Dhanushraj07 <dhanushdharmalingam123@gmail.com>
2024-11-10 11:41:23 +0530

    Fix dynamic port binding for Vercel

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 const {engine} = require('express-handlebars')
 const {loginPage,dashboardPage, loginProcess, attendancePage} = require('./controllers/userControllers')
 const path = require('path')
+const port = process.env.PORT || 8000;
 app.engine('hbs',engine({extname:'hbs',defaultLayout:false}))
 app.set('view engine','hbs');
 app.use(express.static(path.join(__dirname,'public')))
@@ -11,6 +12,6 @@ app.get('/',loginPage);
 app.get('/dashboard',dashboardPage); 
 app.post('/login',loginProcess); 
 app.get('/attendance',attendancePage)
-app.listen(8000,()=>{
-    console.log('Port is running on 8000')
+app.listen(port,()=>{
+    console.log(`Port is running on ${port}`);
 })
