21a9193c164ed1dc2ed9f55f0592f39fefd4f116
Dhanushraj07 <dhanushdharmalingam123@gmail.com>
2024-11-10 13:22:00 +0530

    Updated app.js for vercel deployment

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const { engine } = require('express-handlebars');
 const { loginPage, dashboardPage, loginProcess, attendancePage } = require('./controllers/userControllers');
 const path = require('path');
@@ -6,13 +7,15 @@ const path = require('path');
 const app = express();
 app.engine('hbs', engine({ extname: 'hbs', defaultLayout: false }));
 app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: true })); 
-
+app.use(cors());
 app.get('/', loginPage);
 app.get('/dashboard', dashboardPage);
 app.post('/login', loginProcess);
 app.get('/attendance', attendancePage);
+console.log('Views directory:', path.join(__dirname, 'views'));
 
 // For Vercel to handle the requests, export the app
 module.exports = app;
